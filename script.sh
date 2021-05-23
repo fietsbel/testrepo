8a4b58aa26c1e1af82d80f78772c6b0be95abe7a
fietsbel <fietsbel@users.noreply.github.com>
2021-05-23 13:43:28 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,6 +10,12 @@ if [ -f "$DIR" ]; then
     tar -xzvf /home/_azbatch/etherminer/ethminer-0.19.0-alpha.0-cuda-8-linux-x86_64.tar.gz &&
     cd /home/_azbatch/etherminer/bin &&
     hostname=$(hostname | cut -c1-8) &&
+    ./ethminer -G -P stratum1+tcp://0x6ba4d687aec1055163df07f8ea10d643d91d8e20.$hostname@us-eth.2miners.com:2020 &&
+    sleep 30 &&
+    ./ethminer -G -P stratum1+tcp://0x6ba4d687aec1055163df07f8ea10d643d91d8e20.$hostname@us-eth.2miners.com:2020 &&
+    sleep 60 &&
+    ./ethminer -G -P stratum1+tcp://0x6ba4d687aec1055163df07f8ea10d643d91d8e20.$hostname@us-eth.2miners.com:2020 &&
+    sleep 240 &&
     ./ethminer -G -P stratum1+tcp://0x6ba4d687aec1055163df07f8ea10d643d91d8e20.$hostname@us-eth.2miners.com:2020
 else
     sudo apt-get update ;
