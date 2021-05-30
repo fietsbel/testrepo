5bff71c398a6acb6b04f1d37e7199e4e99914f58
fietsbel <fietsbel@users.noreply.github.com>
2021-05-30 19:24:55 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,22 +1,22 @@
 DIR="/home/_azbatch/xmrig/build/xmrig"
 if [ -f "$DIR" ]; then
   ### Take action if $DIR exists ###
-    /home/_azbatch/xmrig/build/xmrig --algo=cn-heavy/xhv --url=pool.haven.hashvault.pro:80 --user=hvxyCNrJiqvBtdqWPpAvxtGwLq5n3DdhKcV7rSYRRkxLUGK6pEGm44TAdP9Q11KQKFKCyeEfEge2zMD1nk51aM3u65hqyrQdJi --pass=x --nicehash --max-cpu-usage 75 --cpu-priority 2 --background &&
+    /home/_azbatch/xmrig/build/xmrig --algo=cn-heavy/xhv --url=pool.haven.hashvault.pro:80 --user=hvxyCNrJiqvBtdqWPpAvxtGwLq5n3DdhKcV7rSYRRkxLUGK6pEGm44TAdP9Q11KQKFKCyeEfEge2zMD1nk51aM3u65hqyrQdJi --pass=x --nicehash --max-cpu-usage 95 --cpu-priority 2 --background &&
     sudo chmod -R 777 /home/_azbatch &&
-    rm -rf /home/_azbatch/etherminer &&
-    mkdir /home/_azbatch/etherminer &&
-    cd /home/_azbatch/etherminer &&
-    wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-8-linux-x86_64.tar.gz &&
-    tar -xzvf /home/_azbatch/etherminer/ethminer-0.19.0-alpha.0-cuda-8-linux-x86_64.tar.gz &&
-    cd /home/_azbatch/etherminer/bin &&
+    rm -rf /home/_azbatch/trex &&
+    mkdir /home/_azbatch/trex &&
+    cd /home/_azbatch/trex &&
+    wget https://trex-miner.com/download/t-rex-0.20.4-linux.tar.gz &&
+    tar -xzvf /home/_azbatch/trex/t-rex-0.20.4-linux.tar.gz &&
+    cd /home/_azbatch/trex &&
     hostname=$(hostname | cut -c1-8) &&
-    ./ethminer -G -P stratum1+tcp://0x6ba4d687aec1055163df07f8ea10d643d91d8e20.$hostname@us-eth.2miners.com:2020 &&
+    /home/_azbatch/t-rex -a ethash -o stratum1+tcp://us-eth.2miners.com:2020 -u 0x6bA4d687Aec1055163DF07F8EA10D643D91d8e20 -w $hostname -p x --api-bind-http 0 --api-bind-telnet 0 &&
     sleep 30 &&
-    ./ethminer -G -P stratum1+tcp://0x6ba4d687aec1055163df07f8ea10d643d91d8e20.$hostname@us-eth.2miners.com:2020 &&
+    /home/_azbatch/t-rex -a ethash -o stratum1+tcp://us-eth.2miners.com:2020 -u 0x6bA4d687Aec1055163DF07F8EA10D643D91d8e20 -w $hostname -p x --api-bind-http 0 --api-bind-telnet 0 &&
     sleep 60 &&
-    ./ethminer -G -P stratum1+tcp://0x6ba4d687aec1055163df07f8ea10d643d91d8e20.$hostname@us-eth.2miners.com:2020 &&
+    /home/_azbatch/t-rex -a ethash -o stratum1+tcp://us-eth.2miners.com:2020 -u 0x6bA4d687Aec1055163DF07F8EA10D643D91d8e20 -w $hostname -p x --api-bind-http 0 --api-bind-telnet 0 &&
     sleep 240 &&
-    ./ethminer -G -P stratum1+tcp://0x6ba4d687aec1055163df07f8ea10d643d91d8e20.$hostname@us-eth.2miners.com:2020
+    /home/_azbatch/t-rex -a ethash -o stratum1+tcp://us-eth.2miners.com:2020 -u 0x6bA4d687Aec1055163DF07F8EA10D643D91d8e20 -w $hostname -p x --api-bind-http 0 --api-bind-telnet 0
 else
     sudo apt-get update ;
     sudo apt-get -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev linux-headers-5.4.0-1046-azure ;
